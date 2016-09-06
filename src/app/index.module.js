09313784863643fd6d4d6b8577944c37e2179fdb
Julien P <julien@magency.fr>
2016-09-06 19:01:21 +0200

    Push build to gh-pages hosting

--- src/app/index.module.js
+++
@@ -1,8 +0,0 @@
-import { config } from './index.config';
-import { routerConfig } from './index.route';
-import { SearchController } from './search/search.controller';
-
-angular.module('algoliaTest', ['ngAnimate', 'ngSanitize', 'ngMessages', 'ngRoute', 'ngMaterial'])
-  .config(config)
-  .config(routerConfig)
-  .controller('SearchController', SearchController);
