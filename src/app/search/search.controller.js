fcaa5a215d5d10b73a6af0816c734652adb919b4
Julien P <julien@magency.fr>
2016-09-06 19:30:08 +0200

    Add postinstall hook and fix compilation warning

--- src/app/search/search.controller.js
+++ src/app/search/search.controller.js
@@ -2,11 +2,13 @@ import algoliasearch from 'algoliasearch';
 
 export class SearchController {
 
-  constructor($scope, $log, $mdDialog) {
+  constructor($scope, $log, $mdDialog, $document) {
     'ngInject';
 
+    // make angular services usable in all methods
     this.$log = $log;
     this.$mdDialog = $mdDialog;
+    this.$document = $document;
 
     this.client = algoliasearch('TU89584OLH', 'f641bdff3040acca0b1180311d248978');
     this.index = this.client.initIndex('products');
@@ -35,7 +37,7 @@ export class SearchController {
   buy(event, productImage) {
     this.$mdDialog.show(
       this.$mdDialog.alert()
-        .parent(angular.element(document.body))
+        .parent(angular.element(this.$document.body))
         .clickOutsideToClose(true)
         .title('That\'s a good choice!')
         .htmlContent('<p class="text-center md-padding"><img src="' + productImage + '"></p>')
